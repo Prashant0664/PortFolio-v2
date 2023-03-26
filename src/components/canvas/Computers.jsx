e385e615bc80a7a4d88fea1fa2aa730679f4a7ff
Thakran <prashantthakran0664@gmail.com>
2023-03-26 22:56:42 +0530

    First Commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -67,7 +67,7 @@ const ComputersCanvas=()=>{
     gl={{preserveDrawingBuffer:true}}
     >
       <Suspense fallback={<CanvasLoader/>}>
-        <OrbitControls enableZoom={false} 
+        <OrbitControls enableZoom={true} 
         // maxPolarAngle={Math.PI/2}
         // minPolarAngle={Math.PI/2}
         // minAzimuthAngle= {Math.PI/2}
