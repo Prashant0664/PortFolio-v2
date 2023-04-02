6b1b919ab47b7d859f3f551c16f4be2afe42989f
Thakran <prashantthakran0664@gmail.com>
2023-04-02 17:00:59 +0530

    Model added

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Stars = () => {
   return (
-    <div>Stars</div>
+    <div className='stars-div1'>
+      <iframe scrolling="no" className='iframec'loading="lazy" referrerPolicy="no-referrer-when-downgrade"  style={{ border: 0 }} height="100%" width="100%" src="https://earth-3d-model-portfolio.vercel.app/"></iframe>
+    </div>
   )
 }
 
