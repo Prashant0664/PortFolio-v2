942d37836041b87901a4a4f3e5ef6a19d2cccfd5
Prashant <98750053+Prashant0664@users.noreply.github.com>
2023-09-09 16:21:10 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { technologies } from '../constants'
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <>
+    <div className='techmainn'>
+    <div className='techhead'><h1>Tech</h1></div>
+      <div className="techcon">
+        <div className='techcon2'>
+          {technologies.map((i)=>{return (<>
+            <div className='techname'>
+              {i.name}
+            </div>
+          </>)})}
+        </div>
+      </div>
+    
+    </div>
+      </>
   )
 }
 
